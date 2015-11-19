5184202b71ad32d5021d797d1f98b98d37a2bc18
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2015-11-19 15:41:53 -0500

    added manual boostrapping for angular

--- public/app.js
+++ public/app.js
@@ -1,2 +1,10 @@
-var app = angular.module("app", []);
+/// <reference path="../typings/angularjs/angular.d.ts"/>
+var mainApplicationModuleName = "main";
+// declare angular module
+var mainApplicationModule = angular.module(mainApplicationModuleName, []);
+// wait for document to load before bootstrapping angular application
+angular.element(document).ready(function () {
+    // now manually bootstrap the angular app
+    angular.bootstrap(document, [mainApplicationModuleName]);
+});
 //# sourceMappingURL=app.js.map
--- public/app.ts
+++ public/app.ts
@@ -1,1 +1,13 @@
-var app = angular.module("app", []);
+/// <reference path="../typings/angularjs/angular.d.ts"/>
+
+var mainApplicationModuleName:string = "main";
+
+// declare angular module
+var mainApplicationModule:ng.IModule = angular.module(mainApplicationModuleName, []);
+
+// wait for document to load before bootstrapping angular application
+angular.element(document).ready(function() {
+	// now manually bootstrap the angular app
+	angular.bootstrap(document,[mainApplicationModuleName]);
+});
+
